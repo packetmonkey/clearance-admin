59aec7552931c47e751c99dd85cf28f6d519cb39
Evan Sharp <evan@packetmonkey.org>
2011-05-31 11:13:28 -0400

    English, whitespace, and indentation

--- lib/clearance_admin/authentication.rb
+++ lib/clearance_admin/authentication.rb
@@ -1,35 +1,31 @@
 module Clearance
   module Admin
-
     def self.included(controller) # :nodoc:
       controller.send(:include, InstanceMethods)
-
       controller.class_eval do
         helper_method :signed_in_as_admin?
       end
     end
 
     module InstanceMethods
-        # Checks wheter an user has sign in and it's an admin
-        #
-        # @return [true, false] 
-       def signed_in_as_admin?
-         signed_in? && current_user.admin?
-       end
+      # Checks wheter an user has sign in and it's an admin
+      #
+      # @return [true, false]
+      def signed_in_as_admin?
+        signed_in? && current_user.admin?
+      end
 
-       # Denies access for non-users
-       #
-       def users_only
-         deny_access("Necesitas crear una cuenta para entrar a esta sección") unless signed_in?
-       end
- 
-       # Denies access for non-admins
-       #
-       def admin_only
-         deny_access("Necesitas tener privilegios de administrador para entrar.") unless signed_in_as_admin?
-       end
-     
-   end
- 
- end 
+      # Denies access for non-users
+      #
+      def users_only
+        deny_access("You must be signed in to access this page.") unless signed_in?
+      end
+
+      # Denies access for non-admins
+      #
+      def admin_only
+        deny_access("You must be signed in as an admin to access this page.") unless signed_in_as_admin?
+      end
+    end
+  end
 end
